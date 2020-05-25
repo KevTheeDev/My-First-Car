ce9990472597218ce89c6a82a7807a4f8073b25f
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-25 16:42:34 -0500

    React and Spring App, Init Commit

---
+++ myFirstSpringBootCar/src/main/java/com/example/myFirstSpringBootCar/controller/CarController.java
@@ -0,0 +1,23 @@
+//package com.example.myFirstSpringBootCar.controller;
+//
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/my_car")
+//public class CarController {
+//
+//    @Autowired
+//    CarService carService;
+//
+//    @GetMapping
+//    public Iterable<Book> listBooks() {
+//        return bookService.listBooks();
+//    }
+//
+//    @PostMapping
+//    public Book createBook(@RequestBody Book book) {
+//        return bookService.createBook(book);
+//    }
+//}
---
+++ myFirstSpringBootCar/src/main/java/com/example/myFirstSpringBootCar/model/CarTable.java
@@ -0,0 +1,10 @@
+package com.example.myFirstSpringBootCar.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "my_cars")
+public class CarTable {
+
+}
